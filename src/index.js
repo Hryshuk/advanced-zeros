a1ccae3f55ae14ab6e2f52997af02572f987af45
Dmitry Hryshuk <hryshuk@tut.by>
2019-03-19 11:33:02 +0300

    fix: timeout

--- src/index.js
+++ src/index.js
@@ -33,23 +33,49 @@ module.exports = function getZerosCount(number, base) {
 
   console.log(pfBase);
 
-  let key = Math.max(...pfBase.keys());
-  let pfNumber = 0;
+  let key1 = Math.max(...pfBase.keys());
+  let value1 = pfBase.get(key1);
+  let pfNumber1 = 0;
+
+  let key2 = null;
+  let value2 = null;
+  let pfNumber2 = 0;
+
+  pfBase.delete(key1);
+  if (pfBase.size) {
+    key2 = Math.max(...pfBase.keys());
+    value2 = pfBase.get(key2);
+  }
+
+  //let pfNumber = new Map();
 
   for (let n = 1; n <= number; n++) {
-    
-    
+    let num = n;
+
     //for(let key of pfBase.keys()) {
       //let value = getPrimeFactorizationsNumber(n, key);
 
-      /* let num = n;
-      let value = 0;
-      while (num % key == 0) {
-        pfNumber++;
-        num = num / key;
-      } */
+      if (key1) {
+        let value = 0;
+        while (num % key1 == 0) {
+          value++;
+          num = num / key1;
+        }
+        pfNumber1 = pfNumber1 + value;
+      }
+      /* let v = pfNumber.get(key) || 0;
+      pfNumber.set(key, v + value); */
 
-      pfNumber = pfNumber + getPrimeFactorizationsNumber(n, key);
+      if (key2) {
+        let value = 0;
+        while (num % key2 == 0) {
+          value++;
+          num = num / key2;
+        }
+        pfNumber2 = pfNumber2 + value;
+      }
+
+      //pfNumber = pfNumber + getPrimeFactorizationsNumber(n, key);
     //}
   }
 
@@ -60,7 +86,11 @@ module.exports = function getZerosCount(number, base) {
     }
   }); */
 
-  return Math.floor(pfNumber / pfBase.get(key));
+  if (pfNumber1 && pfNumber2 && value1 && value2) {
+    return Math.min(Math.floor(pfNumber1 / value1), Math.floor(pfNumber2 / value2));
+  } else if (pfNumber1 && value1){
+    return Math.floor(pfNumber1 / value1);
+  }
 
   //return Math.min(...values);
 
